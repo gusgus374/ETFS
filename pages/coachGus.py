b571231e2b5d542406e9184f3e9c2de9a8e25939
gusgus374 <gusalvarez374@gmail.com>
2024-07-02 12:13:53 -0400

    added an example

--- pages/coachGus.py
+++ pages/coachGus.py
@@ -7,6 +7,7 @@ import streamlit as st
 import altair as alt
 import streamlit.components.v1 as components
 from streamlit_ace import st_ace
+import time
 
 st.image(image="./resources/ETSF_logo.png",width=60)
 st.title("Welcome to the East Tennessee Freedom Schools footyLab!")
@@ -74,11 +75,11 @@ with col1:
 st.subheader("Soccer and Science.")
 st.header("What about that data thing? What *is* data?")
 
-coach_message = st.chat_message(name="Coach Gus",avatar="./resources/profile_coachGus.JPG")
-coach_message.write("Below you'll find a *file uploader* and the *codeBox* where you can play around with data you upload!")
-with st.echo():
-     uploaded_file = st.file_uploader("Choose a data file")
-     if uploaded_file is not None:
+#coach_message = st.chat_message(name="Coach Gus",avatar="./resources/profile_coachGus.JPG")
+#coach_message.write("Below you'll find a *file uploader* and the *codeBox* where you can play around with data you upload!")
+
+uploaded_file = st.file_uploader("Choose a data file")
+if uploaded_file is not None:
         # use the Pandas read_csv method to read the gps_data and turn into a dataframe
         all_data = pd.read_csv(uploaded_file)
         # keep only the rows were the column 'Split Name' has a value equal to 'all'
@@ -88,8 +89,8 @@ with st.echo():
         #game_data["day"] = game_data["Date"] - 45150
         #game_data = game_data.loc[game_data["day"] > 0]
         with st.expander(label="View Your Data",expanded=False):
-            #display the uploaded data
-            st.write(game_data)
+                #display the uploaded data
+                st.write(game_data)
         variable_x = st.selectbox("Pick Your X Variable!",game_data.columns.to_list(),1)
         variable_y = st.selectbox("Pick Your Y Variable!",game_data.columns.to_list(),8)
         variable_size = st.selectbox("Pick Your Size Variable!",game_data.columns.to_list(),9)
@@ -101,3 +102,38 @@ if uploaded_file is not None:
         color=alt.Color('Player Name',legend=None),
         tooltip=["Player Name","Session Title",]).properties(height=500).interactive()
     st.altair_chart(chart, theme="streamlit", use_container_width=True)
+
+st.divider()
+st.header("CODE EXAMPLES")
+st.subheader("Learn to load a CSV file stored locally")
+with st.expander("~~spellbooks~~ libraries used"):
+       st.code(
+              body= '''
+              import streamlit as st
+              import pandas as pd
+              import os
+              import path
+              ''',
+              language="python"
+       )
+with st.echo():
+        file_location = os.path.join(str(pathlib.Path().resolve()), './data/last30days_GPS.csv')
+        #using the os and path libraries (spellbooks), we save the location of our data file in the variable "file_location"
+        #then we use the "open" spell to take a look at the file location and store the contents inside of the "file" variable. We then use the pandas method, read_csv(), to read the data file and store inside the "data" variable
+        with open(file_location) as file:
+                data = pd.read_csv(file)
+        #then we make sure the data is in the right format (called a type) for us to work with, DataFrame, using the pandas method DataFrame(). We call our pandas DataFrame "ETFS_data"
+        ETFS_data = pd.DataFrame(data)
+        #then we take a look at our data using st.dataframe()
+        st.dataframe(ETFS_data)
+st.subheader("Isolate a single player's data - let's say Ayden")
+with st.echo():
+       
+       aydenData = ETFS_data.loc[ETFS_data["Player Name"] == "Ayden - ETFS"]
+       #the line of code above used the pandas "loc" method. It's asking the computer to keep only the rows of the dataframe where the values in the column "Player Name" match with the string "Ayden - ETFS".
+       #then we ask streamlit to show us our new dataframe using the st.dataframe spell (really called a method)
+       st.dataframe(aydenData)
+coach_message = st.chat_message(name="Coach Gus",avatar="./resources/profile_coachGus.JPG")
+coach_message.write("Here we use the streamlit's line_chart method to plot Ayden's daily top speed")
+with st.echo():
+        st.line_chart(aydenData, x = "Session Title", y="Top Speed (m/s)")
